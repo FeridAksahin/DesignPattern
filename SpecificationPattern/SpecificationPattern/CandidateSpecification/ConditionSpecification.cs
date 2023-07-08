962545c71430d481fe4a9c1afa39a11f99a5fe91
Ferid Akşahin <ferid.aksahin98@gmail.com>
2023-07-08 18:34:11 +0300

    Candidate Filter Specifications

---
+++ SpecificationPattern/SpecificationPattern/CandidateSpecification/ConditionSpecification.cs
@@ -0,0 +1,20 @@
+﻿using SpecificationPattern.Enum;
+using SpecificationPattern.Interface;
+
+namespace SpecificationPattern.CandidateSpecification
+{
+    public class ConditionSpecification : ISpecification<Product>
+    {
+        public Condition Condition;
+
+        public ConditionSpecification(Condition condition)
+        {
+            Condition = condition;
+        }
+
+        public bool IsSatisfied(Product p)
+        {
+            return p.Condition == Condition;
+        }
+    }
+}
---
+++ SpecificationPattern/SpecificationPattern/CandidateSpecification/PopularitySpecification.cs
@@ -0,0 +1,20 @@
+﻿using SpecificationPattern.Enum;
+using SpecificationPattern.Interface;
+
+namespace SpecificationPattern.CandidateSpecification
+{
+    public class PopularitySpecification : ISpecification<Product>
+    {
+        public Popularity Popularity;
+
+        public PopularitySpecification(Popularity popularity)
+        {
+            Popularity = popularity;
+        }
+
+        public bool IsSatisfied(Product p)
+        {
+            return p.Popularity == Popularity;
+        }
+    }
+}
